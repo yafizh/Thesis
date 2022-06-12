ffeffc842b97600a42670ac4baa200b224f8e490
yafizh <a.chairil.yafizh@gmail.com>
2022-06-13 06:17:47 +0800

    add unique method to employee_id on users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('employee_id')->nullable();
+            $table->foreignId('employee_id')->unique()->nullable();
             $table->string('password');
             $table->string('status');
             $table->rememberToken();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Employee;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -25,6 +26,40 @@ class DatabaseSeeder extends Seeder
             'employee_id' => null,
             'password' => bcrypt('admin'),
             'status' => 'super-admin',
+        ]);
+
+        $nip = '198609262015051001';
+        $id = (Employee::create([
+            'nip' => $nip,
+            'name' => 'Ida Faridatul Alawiyah, S.ST',
+            'position' => 'Penyuluh Pertanian Pertama',
+            'sex' => '0',
+            'phone_number' => '0863397364254',
+            'address' => 'Jl Kemang Tmr IV/18, Dki Jakarta',
+            'photo' => ''
+        ]))->id;
+
+        User::create([
+            'employee_id' => $id,
+            'password' => bcrypt($nip),
+            'status' => 'employee',
+        ]);
+
+        $nip = '198609262015051002';
+        $id = (Employee::create([
+            'nip' => $nip,
+            'name' => 'Awanis, S.TP., M.Si',
+            'position' => 'Pascapanen',
+            'sex' => '0',
+            'phone_number' => '086595415871',
+            'address' => 'Perum Taman Elang Blok: N No: 6 Tangerang, Banten',
+            'photo' => ''
+        ]))->id;
+
+        User::create([
+            'employee_id' => $id,
+            'password' => bcrypt($nip),
+            'status' => 'employee',
         ]);
     }
 }
